3733830b2529b89b3435bed2f5205b7b77ccac41
shakilahamedr <shakilahamed0607@gmail.com>
2025-11-05 04:24:37 +0530

    updated client packages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,29 @@
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    './public/index.html',
-    './src/**/*.{js,jsx,ts,tsx}'
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amber: {
+          50: '#fffbeb',
+          100: '#fef3c7',
+          200: '#fde68a',
+          300: '#fcd34d',
+          400: '#fbbf24',
+          500: '#f59e0b',
+          600: '#d97706',
+          700: '#b45309',
+          800: '#92400e',
+          900: '#78350f',
+        },
+      },
+    },
   },
   plugins: [
-    require('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
   ],
-};
+}
